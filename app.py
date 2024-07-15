593db6d00c9a5a4656303af2bb0a1ed12068cb28
Naufal <mohammednaufal2603@gmail.com>
2024-07-15 21:19:42 +0530

    made # of target days dynamic

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     date_one = request.args.get('date', '12/28/2022')  # Default to '12/28/2022' if no date is provided
-    return render_template('index.html', date_one=date_one)
+    target = request.args.get('target', 100)
+    return render_template('index.html', date_one=date_one, target=target)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
